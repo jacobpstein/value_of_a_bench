9b1968d9439c82cb838f4b384fd01f173cdfeb4e
Simon Heitin <simon.heitin@gmail.com>
2023-07-05 22:31:19 -0400

    tweaked the dag so the text fits in the bubbles and you can see the arrows.

--- 01 Code/05 dag.R
+++ 01 Code/05 dag.R
@@ -5,14 +5,14 @@ library(tidyverse)
 our_dag <- dagitty('dag {
   "Bench Performance" [exposure,pos="-1.823,-0.308"]
   "Starter Performance" [adjusted,pos="-2.200,-1.520"]
-  "Team Win Percentage" [outcome,pos="-0.300,-0.082"]
+  "Team Win Pct" [outcome,pos="-0.300,-0.082"]
   Injuries [adjusted,pos="-2.117,0.929"]
-  "Bench Performance" -> "Team Win Percentage" [pos="-0.750,0.047"]
-  "Starter Performance" -> "Team Win Percentage" [pos="-0.791,-1.045"]
+  "Bench Performance" -> "Team Win Pct" [pos="-0.750,0.047"]
+  "Starter Performance" -> "Team Win Pct" [pos="-0.791,-1.045"]
   Injuries -> "Bench Performance"
   Injuries -> "Starter Performance"
-  Injuries -> "Team Win Percentage"
+  Injuries -> "Team Win Pct"
 }')
 
-ggdag(our_dag) +
-  theme_light()
+ggdag(our_dag, text_size = 1.2, node_size = 18) +
+  theme_dag() 
